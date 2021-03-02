3cd8f5070d6bf4a82e88bd70bec81990fe8f7a94
Tushar Saxena <stushar345@gmail.com>
2021-03-02 11:37:52 +0530

    Add files via upload

--- two.cpp
+++ two.cpp
@@ -1,18 +1,78 @@
-vector<int> levelOrder(Node* root)
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
 {
-     queue<Node*>q;
-     vector<int>v;
-        if(root==NULL) return v;
-        q.push(root);
-        while(q.empty()!=true)
-        {   
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {   Node* t=q.front();q.pop();
-                v.push_back(t->data);
-                if(t->left) q.push(t->left);
-                if(t->right) q.push(t->right);
-            }
-        }
-        return v;
-    }
+	int data; 
+	Node* left;
+	Node* right;
+};
+
+Node* GetNewNode(int data) 
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+Node* Insert(Node* root,int data) 
+{
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted is lesser, insert in left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert in right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+
+void level_order_traversal(Node* root)
+{
+	if(root==NULL)
+	return;
+
+	queue<Node*> q;
+	q.push(root);
+	while(q.size())
+	{
+		int size=q.size();
+		for(int i=0;i<size;i++)
+		{
+		Node* temp=q.front();
+		cout<<temp->data<<" ";
+		q.pop();
+		if(temp->left!=NULL)
+		q.push(temp->left);
+		if(temp->right!=NULL)
+		q.push(temp->right);
+		}
+		cout<<endl;
+	}
+	
+}
+
+
+int main() 
+{
+	Node* root = NULL;  // Creating an empty tree
+
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,25);
+	root = Insert(root,8);
+	root = Insert(root,12);
+
+	level_order_traversal(root);
+
+}
