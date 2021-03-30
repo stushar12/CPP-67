55106ba7f2c3302c13f8c7c6800f881fe064376e
Tushar Saxena <stushar345@gmail.com>
2021-03-30 23:26:06 +0530

    Update two.cpp

--- two.cpp
+++ two.cpp
@@ -1,78 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-struct Node 
-{
-	int data; 
-	Node* left;
-	Node* right;
-};
-
-Node* GetNewNode(int data) 
-{
-	Node* newNode = new Node();
-	newNode->data = data;
-	newNode->left = newNode->right = NULL;
-	return newNode;
-}
-
-Node* Insert(Node* root,int data) 
-{
-	if(root == NULL)                                      // empty tree
-    {                                           
-		root = GetNewNode(data);
-	}
-	
-	else if(data <= root->data)                         // if data to be inserted is lesser, insert in left subtree. 
+ vector<int> levelOrder(Node* node)
     {
-		root->left = Insert(root->left,data);
-	}
-	
-	else                                                // else, insert in right subtree. 
+     vector<int> v;
+    queue<Node*> Queue; 
+    Queue.push(node);
+
+
+    while(!Queue.empty())
     {
-		root->right = Insert(root->right,data);
-	}
-	return root;
-}
+        Node* Current_Node = Queue.front();
+        Queue.pop();
+       v.push_back(Current_Node->data);
 
-
-void level_order_traversal(Node* root)
-{
-	if(root==NULL)
-	return;
-
-	queue<Node*> q;
-	q.push(root);
-	while(q.size())
-	{
-		int size=q.size();
-		for(int i=0;i<size;i++)
-		{
-		Node* temp=q.front();
-		cout<<temp->data<<" ";
-		q.pop();
-		if(temp->left!=NULL)
-		q.push(temp->left);
-		if(temp->right!=NULL)
-		q.push(temp->right);
-		}
-		cout<<endl;
-	}
-	
-}
-
-
-int main() 
-{
-	Node* root = NULL;  // Creating an empty tree
-
-	root = Insert(root,15);	
-	root = Insert(root,10);	
-	root = Insert(root,20);
-	root = Insert(root,25);
-	root = Insert(root,8);
-	root = Insert(root,12);
-
-	level_order_traversal(root);
-
-}
+        if(Current_Node->left!=NULL) 
+        Queue.push(Current_Node->left);
+        
+        if(Current_Node->right!=NULL) 
+        Queue.push(Current_Node->right);
+    }
+    return v;
+    }
